1e7d4d271e8d46bb01679335e63307217db41052
Steven Blom <atomicblom@binaryvibrance.net>
2016-01-19 17:58:24 +1030

    Textures for the Button

--- src/main/java/mod/steamnsteel/mcgui/client/gui/controls/ButtonControl.java
+++ src/main/java/mod/steamnsteel/mcgui/client/gui/controls/ButtonControl.java
@@ -3,6 +3,7 @@ package mod.steamnsteel.mcgui.client.gui.controls;
 import mod.steamnsteel.mcgui.client.gui.Control;
 import mod.steamnsteel.mcgui.client.gui.GuiLogger;
 import mod.steamnsteel.mcgui.client.gui.GuiRenderer;
+import mod.steamnsteel.mcgui.client.gui.GuiTexture;
 import mod.steamnsteel.mcgui.client.gui.events.IButtonPressedEventListener;
 import org.lwjgl.util.ReadablePoint;
 import org.lwjgl.util.Rectangle;
@@ -11,6 +12,12 @@ import java.util.List;
 
 public class ButtonControl extends Control
 {
+    private GuiTexture defaultTexture;
+    private GuiTexture pressedTexture;
+    private GuiTexture hoverTexture;
+    private GuiTexture disabledTexture;
+    private GuiTexture currentTexture;
+
     public ButtonControl(GuiRenderer guiRenderer)
     {
         super(guiRenderer);
@@ -27,11 +34,33 @@ public class ButtonControl extends Control
     }
 
     @Override
+    public void draw()
+    {
+        super.draw();
+        if (currentTexture != null)
+        {
+            guiRenderer.drawComponentTexture(this, currentTexture);
+        }
+    }
+
+    @Override
     protected boolean onMouseRelease(ReadablePoint point, int mouseButton)
     {
         if (mouseButton == 0)
         {
             onButtonPressedInternal();
+            currentTexture = defaultTexture;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean onMouseClick(ReadablePoint point, int mouseButton)
+    {
+        if (mouseButton == 0)
+        {
+            currentTexture = pressedTexture;
             return true;
         }
         return false;
@@ -71,5 +100,49 @@ public class ButtonControl extends Control
     @SuppressWarnings("unused")
     public void removeOnButtonPressedEventListener(IButtonPressedEventListener listener) {
         buttonPressedEventListeners.remove(listener);
+    }
+
+
+    public GuiTexture getDefaultTexture()
+    {
+        return defaultTexture;
+    }
+
+    public void setDefaultTexture(GuiTexture defaultTexture)
+    {
+        if (this.currentTexture == this.defaultTexture) {
+            this.currentTexture = defaultTexture;
+        }
+        this.defaultTexture = defaultTexture;
+    }
+
+    public GuiTexture getPressedTexture()
+    {
+        return pressedTexture;
+    }
+
+    public void setPressedTexture(GuiTexture pressedTexture)
+    {
+        this.pressedTexture = pressedTexture;
+    }
+
+    public GuiTexture getHoverTexture()
+    {
+        return hoverTexture;
+    }
+
+    public void setHoverTexture(GuiTexture hoverTexture)
+    {
+        this.hoverTexture = hoverTexture;
+    }
+
+    public GuiTexture getDisabledTexture()
+    {
+        return disabledTexture;
+    }
+
+    public void setDisabledTexture(GuiTexture disabledTexture)
+    {
+        this.disabledTexture = disabledTexture;
     }
 }
